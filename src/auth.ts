ae521d80782f121118a597c9908fe8a6b6862405
laurhakob <laurhakob@gmail.com>
2026-05-28 23:42:32 +0400

    basics of Auth js and sign in with github

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,9 @@
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github"
 
-// import authConfig from "@/auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: []
+    providers: [GitHub]
 });
 
 
-
-
-// import NextAuth from "next-auth";
-
-// import authConfig from "@/auth.config";
-
-// export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
